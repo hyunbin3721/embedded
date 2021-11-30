a3d91862e21bdad6ae5a8bb50d88f321ceb3e3b4
hyunbin <hyunvin37@naver.com>
2021-11-30 17:28:09 +0900

    수정

--- week12/ledtest.c
+++ week12/ledtest.c
@@ -18,23 +18,20 @@ int main(void)
     int ledStat = 0;
     int i=0;
 
-    ledLibInit();   // led initailize
+    ledLibInit();
 
 	for (i=0;i<8;i++)
 	{
 		ledOnOff(i, 1);		// i번째 led 키기
 		sleep(1);
 		ledStat = ledStatus();
-		printf("ledStatus is : %02x\n", ledStat);		
+		printf("led Current Status: %02x\n", ledStat);	
 		ledOnOff(i,0);		// i번째 led 끄기
 	}
 
     sleep(1);
-	printf("Answer is correct : All led On!\n");
+	printf("All led On\n");
 	ledifAns();
-	
-
-    ledLibExit(); 	// led Exit
-
+    ledLibExit();
     return 0;
 }
