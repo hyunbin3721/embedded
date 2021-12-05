fe3b022765f8722b98ecd090dec672faa85b1068
Lee_woong_hee <morning772@naver.com>
2021-12-05 23:33:28 +0900

    Button
    Button

--- week12/button.c
+++ week12/button.c
@@ -18,13 +18,17 @@
 
 #define HAVE_TO_FIND_1 "N: Name=\"ecube-button\"\n"
 #define HAVE_TO_FIND_2 "H: Handlers=kbd event"
+
+
 pthread_t button_t;
+
 
 static int fp;
 static int readSize, inputIndex;
 struct input_event stEvent;
 static char inputDevPath[200]={0,};
-int msgID;
+static int msgID;
+static int status;
 
 int probeButtonPath(char *newPath)
 {
@@ -38,14 +42,11 @@ int probeButtonPath(char *newPath)
 		fgets(tmpStr,200,fp);
 		if(strcmp(tmpStr,HAVE_TO_FIND_1)==0)
 		{
-			//printf("event is %s\r\n", tmpStr);
 			returnValue = 1;
 		}
 		if((returnValue == 1) && (strncasecmp(tmpStr, HAVE_TO_FIND_2,strlen(HAVE_TO_FIND_2)) == 0))
 		{
-			//printf("-->%s",tmpStr);
-			//printf("\t%c\r\n",tmpStr[strlen(tmpStr)-3]);
-			//number = tmpStr[strlen(tmpStr)-3] - '0';
+			number = tmpStr[strlen(tmpStr)-3] - '0';
 			break;
 		}
 	}
@@ -58,50 +59,55 @@ void* button_read(void *arg)
 {
 	BUTTON_MSG_T sendButton;
 	
+	msgID = msgget ((key_t)MESSAGE_ID, IPC_CREAT|0666);
+	if(msgID == -1)
+	{
+		printf("msgget failed!\n");
+		return 0;
+	}
+	
 	while(1)
 	{
 		readSize = read(fp, &stEvent, sizeof(stEvent));
 		if(readSize != sizeof(stEvent))continue;
 		if(stEvent.type == EV_KEY)
 		{
-			//printf("EV_KEY(");
 			switch(stEvent.code)
 			{
 				case KEY_VOLUMEUP: 
 				sendButton.messageNum = 1;
 				sendButton.keyInput = 1;
 				
-				//printf("VOLUP "); 
 				break;
 				
 				case KEY_HOME: 
 				sendButton.messageNum = 1;
 				sendButton.keyInput = 2;
-				//printf("HOME "); 
+
 				break;
 				
 				case KEY_SEARCH: 
 				sendButton.messageNum = 1;
 				sendButton.keyInput = 3;
-				//printf("SEARCH "); 
+
 				break;
 				
 				case KEY_BACK: 
 				sendButton.messageNum = 1;
 				sendButton.keyInput = 4;
-				//printf("BACK "); 
+
 				break;
 				
 				case KEY_MENU: 
 				sendButton.messageNum = 1;
 				sendButton.keyInput = 5;
-				//printf("MENU "); 
+
 				break;
 				
 				case KEY_VOLUMEDOWN: 
 				sendButton.messageNum = 1;
 				sendButton.keyInput = 6;
-				//printf("VOLDOWN "); 
+
 				break;
 			}
 			
@@ -115,6 +121,9 @@ void* button_read(void *arg)
 				sendButton.pressed = 0;
 				msgsnd(msgID, &sendButton, sizeof(sendButton.pressed)+sizeof(sendButton.keyInput), 0);
 			}
+			usleep(1);
+			msgrcv(msgID, &sendButton, sizeof(sendButton.pressed)+sizeof(sendButton.keyInput), 0,IPC_NOWAIT);
+			if(sendButton.keyInput == 10)break;
 		}
 	}
 }
@@ -127,28 +136,22 @@ int buttonInit(void)
 		printf("fp Error!\n");
 		return 0;
 	}
-	printf("init2\n");
-	//printf("inputDevPath: %s\r\n", inputDevPath);
+
 	fp = open(inputDevPath, O_RDONLY);
+	
 	if(fp < 0)printf("open failed!\n");
-	msgID = msgget ((key_t)MESSAGE_ID, IPC_CREAT|0666);
-	if(msgID == -1)
-	{
-		printf("msgget failed!\n");
-		return 0;
-	}
-	printf("init2\n");
+	
 	if((status = pthread_create(&button_t, NULL, &button_read, NULL)) != 0)
 	{
         printf("Button_t failed!\n");
         return 0;
     }
-    printf("init2\n");
+
     return 1;
 }
 
 int buttonExit(void)
 {
-	pthread_join(button_t, NULL);
 	close(fp);
+	pthread_join(button_t, NULL);
 }
--- week12/buttontest.c
+++ week12/buttontest.c
@@ -12,25 +12,24 @@
 #include <sys/msg.h>
 #include "button.h"
 
+BUTTON_MSG_T recieveButton;
+
 int main(void)
 {
-	int status = 0;
 	int msgID;
-	BUTTON_MSG_T recieveButton;
+	int status = 0;
 
-	printf("init\n");
-	if((status = msgget((key_t)MESSAGE_ID, IPC_CREAT|0666)) != 0)
+	
+	if((msgID = msgget((key_t)MESSAGE_ID, IPC_CREAT|0666)) != 0)
 	{
         printf("msgget failed!\n");
         exit(-1);
     }
-    printf("init\n");
-    if(buttonInit() != 1)printf("buttonInit Failed!\n");exit(-1);
-	printf("init\n");
     
+    if(buttonInit() != 1)printf("buttonInit Failed!\n");
+	
     while(1)
     {
-		printf("test\n");
 		msgrcv(msgID, &recieveButton, sizeof(recieveButton.pressed)+sizeof(recieveButton.keyInput), 0,0);
 		switch(recieveButton.keyInput)
 		{
@@ -44,7 +43,11 @@ int main(void)
 		
 		if(recieveButton.pressed)printf("pressed.\n");
 		else printf("released\n");
+		if(status == 1)break;
 	}
+	
+	buttonExit();
+	return 0;
 }
     
     
