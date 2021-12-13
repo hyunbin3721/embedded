ef4f56442809e2a52357df555797c78edbac6e38
Lee_woong_hee <morning772@naver.com>
2021-12-13 21:32:20 +0900

    14주차 LM74
    14주차 온도센서 LM74 함수, 테스트 앱

---
+++ week14/tempTest.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "temp.h"
+
+int main(void)
+{
+	double thermal; int cnt;
+	if(temp_init() != 1)
+	{
+		printf("thermal_init error!\n");
+		exit(-1);
+	}
+	printf("hello! I'll measure and show Temperature for 10 seconds!\n\n");
+	
+	for(cnt = 0; cnt < 10; cnt++)
+	{
+		thermal = temp_read();
+		
+		printf("Temperature is: %f\n", thermal);
+		
+		sleep(1);
+	}
+	
+	printf("\n");
+	
+	if(temp_exit() != 0)
+	{
+		printf("exit error!\n");
+		exit(-1);
+	}
+	
+	printf("goodbye!\n");
+	
+	return 0;
+}
