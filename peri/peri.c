7f6103dad618b38906eb6af1941cd3401c11be0c
Lee_woong_hee <morning772@naver.com>
2021-12-16 17:07:44 +0900

    peri

--- peri/peri.c
+++ peri/peri.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <ctype.h>
+#include <dirent.h>
+#include <errno.h>
 #include <sys/ipc.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -10,15 +12,15 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/msg.h>
-//#include <sys/stat.h>
-//#include <sys/mman.h>
-//#include <mad.h>
-//#include <pulse/simple.h>
-//#include <pulse/error.h>
-//#include <alsa/asoundlib.h>
-//#include <alsa/mixer.h>
-//#include <
-//#include <
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include "./mad/mad.h"//실제 라이브러리 추가 필요
+#include <pulse/simple.h>
+#include <pulse/error.h>
+#include <alsa/asoundlib.h>
+#include <alsa/mixer.h>
 #include "FND.h"
 #include "led.h"
 #include "button.h"
@@ -29,14 +31,38 @@
 #include "accelMagGyro.h"
 
 #define MP3_PATH "/home/ecube/mp3/"
+#define MEMADD 1234//공유메모리 주소
 
-static pthread_t player;
-static int fd  = 0;
+//PWM----------------------------------------------------------
+#define COLOR_LED_DEV_R_ "/sys/class/pwm/pwmchip0/"
+#define COLOR_LED_DEV_G_ "/sys/class/pwm/pwmchip1/"
+#define COLOR_LED_DEV_B_ "/sys/class/pwm/pwmchip2/"
+
+#define PWM_EXPORT "export"
+#define PWM_UNEXPORT "unexport"
+#define PWM_DUTY "pwm0/duty_cycle"
+#define PWM_PERIOD "pwm0/period"
+#define PWM_ENABLE "pwm0/enable"
+
+#define PWM_COLOR_R 0
+#define PWM_COLOR_G 1
+#define PWM_COLOR_B 2
+//----------------------------------------------------------------------
+
+
 static int msgID;
-static int status = 0;
+static int forkStatus;
+static pid_t pid;
+
+static int fd  = 0;
+static char *tmpBuff;
+static char fileBuf[2000];
+char nameBuf[100][16];
+static int listIndex = 0;
 static int LEDstatus = 0;
 static int VOLstatus;
 static int textLcdstatus = 0;
+static int homeStatus = 0;
 
 BUTTON_MSG_T recieveButton;
 
@@ -47,64 +73,106 @@ int HOME(void);
 int SEARCH(void);
 int BACK(void);
 int MENU(void);
+int decode(void);
 
 int main(void)
 {
+	DIR *list;
+	struct dirent *dir;
+	list = opendir(MP3_PATH);
 	
-	ledLibInit();ledMid();//LED시작, LED1,2,3,4,5 ON
-	pwmLedInit();
-	textlcdInit();
-	FND_init();
-	
-	if(buttonInit() != 1)printf("buttonInit Failed!\n");
-	
-	fd = open(MP3_PATH "/list.txt", O_RDONLY); //곡 리스트 Open
-	if(fd < 0)
+	if(list == NULL)
 	{
-		printf("list open error!\n");
+		printf("mp3 directory open failed!\n");
 		exit(-1);
 	}
 	
-	while(1)//버튼입력 시작.
-    {
-		msgrcv(msgID, &recieveButton, sizeof(recieveButton.pressed)+sizeof(recieveButton.keyInput), 0,0);
-		if(recieveButton.pressed)printf("pressed.\n");
-		
-		else 
+	while((dir = readdir(list)))
+	{
+		if(dir->d_type == DT_REG)
 		{
-			printf("released\n");
-			switch(recieveButton.keyInput)
-			{
-				case 1:
-						VOLUP();//음량 높임
-						break;
-					 
-				case 2: 
-						HOME();//첫 번째 곡으로 이동
-						break;
-					 
-				case 3:
-						SEARCH();//다음 곡
-						break;
-					 
-				case 4:
-						BACK();//이전 곡
-						break;
-					 
-				case 5:
-						MENU();//재생, 정지
-						break;
-			
-				case 6:
-						VOLDOWN();//음량 낮춤
-						break;
-			}
+			memcpy(nameBuf[listIndex],dir->d_name,16);
+			printf("buf: %s\n",nameBuf[listIndex]); 
+			listIndex++;
 		}
-		//if(status == 1)break;
 	}
 	
+	closedir(list);
 	
-	buttonExit();
+	printf("before fork buffer is %s\n", nameBuf[0][0]);
+	pid = fork();
+		
+	if(pid == 0)
+	{
+		int shmID = shmget((key_t)MEMADD, 1, IPC_CREAT|0666);
+		if(shmID == -1)
+		{
+			printf("shmget error!\n");
+			exit(-1);
+		}
+		char *shmemAddr;
+		
+		
+		decode();//디코더 실행
+	}
+	
+	else if(pid > 0)
+	{
+		ledLibInit();ledMid();//LED시작, LED1,2,3,4,5 ON
+		pwmLedInit();
+		printf("buffer is %s\n", nameBuf[0]);
+		textlcdInit();textlcdclear();lcdtextwrite(nameBuf[0]," ", 1);
+		FND_init();FND_off();
+	
+		if(buttonInit() != 1)printf("buttonInit Failed!\n");
+		
+		while(1)//버튼입력 시작.
+		{
+			msgrcv(msgID, &recieveButton, sizeof(recieveButton.pressed)+sizeof(recieveButton.keyInput), 0,0);
+			if(recieveButton.pressed)printf("pressed.\n");
+			
+			else 
+			{
+				printf("released\n");
+				switch(recieveButton.keyInput)
+				{
+					case 1:
+							VOLUP();//음량 높임
+							break;
+					 
+					case 2: 
+							HOME();//첫 번째 곡으로 이동
+							break;
+					 
+					case 3:
+							SEARCH();//다음 곡
+							break;
+					 
+					case 4:
+							BACK();//이전 곡
+							break;
+					 
+					case 5:
+							MENU();//재생, 정지
+							break;
+			
+					case 6:
+							VOLDOWN();//음량 낮춤
+							break;
+				}
+			}
+		//if(status == 1)break;
+		}
+	
+		wait(&forkStatus);
+		buttonExit();
+	}
+	
+	else
+	{
+		printf("Fork failed!\n");
+		exit(-1);
+	}
 }
 
 int ledMid(void)
@@ -114,14 +182,14 @@ int ledMid(void)
 		ledOnOff(ledCnt,1);
 	}
 	
-	LEDstatus = 4;
+	LEDstatus = 3;
 }
 
 int VOLUP(void)
 {
-	ledOnOff(LEDstatus,1);
 	LEDstatus++;
 	if(LEDstatus > 7)LEDstatus = 7;
+	ledOnOff(LEDstatus,1);
 }
 
 int VOLDOWN(void)
@@ -133,25 +201,60 @@ int VOLDOWN(void)
 
 int HOME(void)
 {
+	int maxIndex = sizeof(nameBuf[0]);
+	
+	if(homeStatus == 1)
+	{
+		lcdtextwrite(nameBuf[0], " ",1);
+		homeStatus = 0;
+	}
+	else
+	{
+		lcdtextwrite(nameBuf[maxIndex], " ",1);
+	}
 	printf("to first music or last\n");
-	//pthread로 mp3 디코더 호출
 	
 }
 
 int MENU(void)
 {
 	printf("song should played button\n");
-	//pthread로 mp3 디코더 호출
 	
 }
 
 int BACK(void)
 {
-		lcdtextwrite("song name back", " ", 1);
-				
+	listIndex--;
+	if(listIndex < 0)listIndex = 0;
+	lcdtextwrite(nameBuf[listIndex], " ", 1);			
 }
 
 int SEARCH(void)
 {
-	lcdtextwrite("song name search", " ", 1);
+	int maxIndex = sizeof(nameBuf[0]);
+	listIndex++;
+	if(listIndex > maxIndex)listIndex = maxIndex;
+	lcdtextwrite(nameBuf[listIndex], " ", 1);
+}
+
+int decode(void)
+{
+	int playing = 1;
+	
+	while(playing)//디코딩,재생
+	{
+		printf("testing!\n");
+		for(int i=0; i<3; i++)
+		{   
+			pwmSetPercent(0,i);
+			printf("color change\n");        
+			sleep(1);
+			pwmSetPercent(50,i);
+			printf("color change\n");
+			sleep(1);
+			pwmSetPercent(100,i);
+			printf("color change\n");
+			sleep(1);   
+		}
+	}
 }
